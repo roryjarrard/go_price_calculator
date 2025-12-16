bab3c90ff202cba05429006adb22db9c07b24941
Rory Jarrard <roryjarrard@gmail.com>
2025-12-16 14:38:22 -0700

    Create file manager for IO

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -8,9 +8,14 @@ import (
 	"os"
 )
 
+type FileManager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
 // ReadLines reads lines from a file and returns them as a slice of strings.
-func ReadLines(filePath string) ([]string, error) {
-	file, err := os.Open(filePath)
+func (fm FileManager) ReadLines() ([]string, error) {
+	file, err := os.Open(fm.InputFilePath)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return nil, errors.New("failed to open file")
@@ -34,8 +39,8 @@ func ReadLines(filePath string) ([]string, error) {
 	return lines, nil
 }
 
-func WriteJSON(path string, data interface{}) error {
-	file, err := os.Create(path)
+func (fm FileManager) WriteResult(data interface{}) error {
+	file, err := os.Create(fm.OutputFilePath)
 	if err != nil {
 		return errors.New("failed to create file")
 	}
@@ -48,4 +53,11 @@ func WriteJSON(path string, data interface{}) error {
 	}
 
 	return nil
+}
+
+func New(inputPath, outputPath string) FileManager {
+	return FileManager{
+		InputFilePath:  inputPath,
+		OutputFilePath: outputPath,
+	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -9,6 +9,7 @@ import (
 
 // TaxIncludedPriceJob represents a job to calculate tax-included prices.
 type TaxIncludedPriceJob struct {
+	IOManager         filemanager.FileManager
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]string
@@ -16,7 +17,7 @@ type TaxIncludedPriceJob struct {
 
 // LoadData loads prices from a file and converts them to float64.
 func (job *TaxIncludedPriceJob) LoadData() {
-	lines, err := filemanager.ReadLines("prices.txt")
+	lines, err := job.IOManager.ReadLines()
 	if err != nil {
 		fmt.Println("Error loading lines from file:", err)
 		return
@@ -44,18 +45,13 @@ func (job *TaxIncludedPriceJob) Process() {
 
 	job.TaxIncludedPrices = result
 
-	err := filemanager.WriteJSON(fmt.Sprintf("tax_included_prices_%.2f.json", job.TaxRate), result)
-	if err != nil {
-		fmt.Println("Error writing JSON file:", err)
-		return
-	}
-
-	fmt.Printf("Processed tax-included prices with tax rate %.2f\n", job.TaxRate)
+	job.IOManager.WriteResult(job)
 }
 
 // NewTaxIncludedPriceJob creates a new TaxIncludedPriceJob with the given tax rate.
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   fm,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
